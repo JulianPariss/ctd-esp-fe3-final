add797007bb895c075a06ebfeae89dd61fc599f3
Julian Paris <julian.pariss@hotmail.com>
2023-04-12 20:56:36 -0300

    ..

--- pages/checkout/index.spec.tsx
+++ pages/checkout/index.spec.tsx
@@ -2,7 +2,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Checkout from "./index.page";
 import { comicMock } from "dh-marvel/test/mocks/comic";
-
+import { getServerSideProps } from "../index.page";
+import { GetServerSidePropsContext } from "next/types";
+import { ParsedUrlQuery } from "node:querystring";
 
 describe("ComicIDPage", () => {
   describe("when rendering default page", () => {
@@ -12,6 +14,13 @@ describe("ComicIDPage", () => {
       );
       const title = screen.getByText("Marvel Previews (2017)");
       expect(title).toBeInTheDocument();
+    });
+    it("check on good case", () => {
+      const context = {
+          params: { id: "82967" } as ParsedUrlQuery
+      };
+      const value =  getServerSideProps(context as GetServerSidePropsContext);
+      expect(value).toEqual({props: {name: "Marvel Previews (2017)"}});
     });
   });
 });
